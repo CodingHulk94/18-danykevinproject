3b4efd82a646c641a3441ee0640a812b1e41add7
Dany L <d-lo@outlook.de>
2017-07-21 10:07:23 +0200

    Addition de la partie Nombre de films par catégories dans la page d'accueil

---
+++ view/home.php
@@ -0,0 +1,5 @@
+<ul id="films_per_category">
+    <?php foreach($Result as $key => $value) : ?>
+        <li><a href="catalogue.php?page=<?=$value['cat_id']?>"><?=$value['cat_name']?> (<?=$value['nbmov']?>)</a></li>
+    <?php endforeach; ?>
+</ul>
