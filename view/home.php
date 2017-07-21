f1f0adf756557e8f12c058cd361bc9b1189ffab8
Dany L <d-lo@outlook.de>
2017-07-21 10:55:52 +0200

    Catalogue pour afficher les films selon catégorie sélectionnée

--- view/home.php
+++ view/home.php
@@ -1,5 +1,5 @@
 <ul id="films_per_category">
     <?php foreach($Result as $key => $value) : ?>
-        <li><a href="catalogue.php?page=<?=$value['cat_id']?>"><?=$value['cat_name']?> (<?=$value['nbmov']?>)</a></li>
+        <li><a href="catalogue.php?category=<?=$value['cat_id']?>"><?=$value['cat_name']?> (<?=$value['nbmov']?>)</a></li>
     <?php endforeach; ?>
 </ul>
