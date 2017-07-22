50232a952a0a70f4f05a59c8c0fe3f677dd7e1ce
Dany Lopes <d-lo@outlook.de>
2017-07-22 22:29:44 +0200

    Tri par d’ajout + Fix: Boutons Suivant et Précédent disparaissent lorsque la requête n’envoie aucun film.

--- public/ajax/catalogue.php
+++ public/ajax/catalogue.php
@@ -8,123 +8,249 @@ if(!empty($_POST)){
     $wordSearch = isset($_POST['motrecherche']) ? strip_tags(trim($_POST['motrecherche'])) : '';
     $categoryID = isset($_POST['categoryID']) ? intval($_POST['categoryID']) : 0;
     $CategoryFilter = isset($_POST['filter']) ? intval($_POST['filter']) : 0;
+    $DateFilter = isset($_POST['datefilter']) ? intval($_POST['datefilter']) : 1;
     // JE calcule l'offset correspondant à la page
 
     $offset = $pageajax * 3 - 3;
 
     //Requête
-    if($CategoryFilter === 0){
-        if($validatorvalue === 4){
+    if($DateFilter === 1){
+      if($CategoryFilter === 0){
+          if($validatorvalue === 4){
 
-            $sql = 'SELECT *
-            FROM movies
-            LEFT OUTER JOIN (
-                SELECT COUNT(*) AS filmtotal FROM movies
-            ) AS T ON filmtotal > 0
-            LIMIT 3
-            OFFSET '.$offset.'';
+              $sql = 'SELECT *
+              FROM movies
+              LEFT OUTER JOIN (
+                  SELECT COUNT(*) AS filmtotal FROM movies
+              ) AS T ON filmtotal > 0
+              ORDER BY mov_add_date DESC
+              LIMIT 3
+              OFFSET '.$offset.'';
 
-        }
-        else if($validatorvalue === 5){
-            $sql = 'SELECT *
-            FROM movies
-            LEFT OUTER JOIN (
-                SELECT COUNT(*) AS filmtotal FROM movies WHERE category_cat_id = :catid
-            ) AS T ON filmtotal > 0
-            WHERE category_cat_id = :catid
-            LIMIT 3
-            OFFSET '.$offset.'';
+          }
+          else if($validatorvalue === 5){
+              $sql = 'SELECT *
+              FROM movies
+              LEFT OUTER JOIN (
+                  SELECT COUNT(*) AS filmtotal FROM movies WHERE category_cat_id = :catid
+              ) AS T ON filmtotal > 0
+              WHERE category_cat_id = :catid
+              ORDER BY mov_add_date DESC
+              LIMIT 3
+              OFFSET '.$offset.'';
 
-            $pdoStatement = $pdo->prepare($sql);
+              $pdoStatement = $pdo->prepare($sql);
 
-            $pdoStatement->bindValue(':catid', $categoryID);
+              $pdoStatement->bindValue(':catid', $categoryID);
 
-            if ($pdoStatement->execute() === false ) {
-                print_r($pdoStatement->errorInfo());
-            }
-            else {
-                $AllResults = $pdoStatement->fetchall(PDO::FETCH_ASSOC);
-                echo json_encode($AllResults, JSON_PRETTY_PRINT);
-                // print_r($AllResults);
-            }
+              if ($pdoStatement->execute() === false ) {
+                  print_r($pdoStatement->errorInfo());
+              }
+              else {
+                  $AllResults = $pdoStatement->fetchall(PDO::FETCH_ASSOC);
+                  echo json_encode($AllResults, JSON_PRETTY_PRINT);
+                  // print_r($AllResults);
+              }
 
-        }
-        else if ($validatorvalue === 6){
-            $sql = 'SELECT *
-            FROM movies
-            INNER JOIN category ON category_cat_id = cat_id
-            LEFT OUTER JOIN (
-                SELECT COUNT(*) AS filmtotal
-                FROM movies
-                INNER JOIN category ON category_cat_id = cat_id
-                WHERE mov_title LIKE :word
-                OR mov_synopsis LIKE :word
-                OR cat_name LIKE :word
-            ) AS T ON filmtotal > 0
-            WHERE mov_title LIKE :word
-            OR mov_synopsis LIKE :word
-            OR cat_name LIKE :word
-            LIMIT 3
-            OFFSET '.$offset.'';
+          }
+          else if ($validatorvalue === 6){
+              $sql = 'SELECT *
+              FROM movies
+              INNER JOIN category ON category_cat_id = cat_id
+              LEFT OUTER JOIN (
+                  SELECT COUNT(*) AS filmtotal
+                  FROM movies
+                  INNER JOIN category ON category_cat_id = cat_id
+                  WHERE mov_title LIKE :word
+                  OR mov_synopsis LIKE :word
+                  OR cat_name LIKE :word
+              ) AS T ON filmtotal > 0
+              WHERE mov_title LIKE :word
+              OR mov_synopsis LIKE :word
+              OR cat_name LIKE :word
+              ORDER BY mov_add_date DESC
+              LIMIT 3
+              OFFSET '.$offset.'';
 
-            $pdoStatement = $pdo->prepare($sql);
+              $pdoStatement = $pdo->prepare($sql);
 
-            $pdoStatement->bindValue(':word', "%{$wordSearch}%");
+              $pdoStatement->bindValue(':word', "%{$wordSearch}%");
 
-            if ($pdoStatement->execute() === false ) {
-                print_r($pdoStatement->errorInfo());
-            }
-            else {
-                $AllResults = $pdoStatement->fetchall(PDO::FETCH_ASSOC);
-                echo json_encode($AllResults, JSON_PRETTY_PRINT);
-                // print_r($AllResults);
-            }
+              if ($pdoStatement->execute() === false ) {
+                  print_r($pdoStatement->errorInfo());
+              }
+              else {
+                  $AllResults = $pdoStatement->fetchall(PDO::FETCH_ASSOC);
+                  echo json_encode($AllResults, JSON_PRETTY_PRINT);
+                  // print_r($AllResults);
+              }
 
-            // Calculer le nombre de résultats trouvés
-            $numberOfResults = 0;
-            foreach ($AllResults as $key => $value) {
-                $numberOfResults += 1;
-            }
-            $phraseRecherche = $numberOfResults.' résultat(s) trouvé(s)';
-        }
+              // Calculer le nombre de résultats trouvés
+              $numberOfResults = 0;
+              foreach ($AllResults as $key => $value) {
+                  $numberOfResults += 1;
+              }
+              $phraseRecherche = $numberOfResults.' résultat(s) trouvé(s)';
+          }
 
-        if($validatorvalue !== 5 && $validatorvalue !== 6){
+          if($validatorvalue !== 5 && $validatorvalue !== 6){
 
-            $ResultSet = $pdo->query($sql);
+              $ResultSet = $pdo->query($sql);
 
 
-            if($ResultSet === false){
-                print_r($pdo->errorInfo());
-            }else{
-                $AllResults = $ResultSet->fetchAll(PDO::FETCH_ASSOC);
-                echo json_encode($AllResults, JSON_PRETTY_PRINT);
-            }
+              if($ResultSet === false){
+                  print_r($pdo->errorInfo());
+              }else{
+                  $AllResults = $ResultSet->fetchAll(PDO::FETCH_ASSOC);
+                  echo json_encode($AllResults, JSON_PRETTY_PRINT);
+              }
 
-        }
+          }
+      }
+      else{
+          $sql = 'SELECT *
+          FROM movies
+          LEFT OUTER JOIN (
+              SELECT COUNT(*) AS filmtotal FROM movies WHERE category_cat_id = :catfilter
+          ) AS T ON filmtotal > 0
+          WHERE category_cat_id = :catfilter
+          ORDER BY mov_add_date ASC
+          LIMIT 3
+          OFFSET '.$offset.'';
+
+          $pdoStatement = $pdo->prepare($sql);
+
+          $pdoStatement->bindValue(':catfilter', $CategoryFilter);
+
+          if ($pdoStatement->execute() === false ) {
+              print_r($pdoStatement->errorInfo());
+          }
+          else {
+              $AllResults = $pdoStatement->fetchall(PDO::FETCH_ASSOC);
+              echo json_encode($AllResults, JSON_PRETTY_PRINT);
+              // print_r($AllResults);
+          }
+
+      }
     }
-    else{
-        $sql = 'SELECT *
-        FROM movies
-        LEFT OUTER JOIN (
-            SELECT COUNT(*) AS filmtotal FROM movies WHERE category_cat_id = :catfilter
-        ) AS T ON filmtotal > 0
-        WHERE category_cat_id = :catfilter
-        LIMIT 3
-        OFFSET '.$offset.'';
+    else if($DateFilter === 2){
+      if($CategoryFilter === 0){
+          if($validatorvalue === 4){
 
-        $pdoStatement = $pdo->prepare($sql);
+              $sql = 'SELECT *
+              FROM movies
+              LEFT OUTER JOIN (
+                  SELECT COUNT(*) AS filmtotal FROM movies
+              ) AS T ON filmtotal > 0
+              ORDER BY mov_add_date ASC
+              LIMIT 3
+              OFFSET '.$offset.'';
 
-        $pdoStatement->bindValue(':catfilter', $CategoryFilter);
+          }
+          else if($validatorvalue === 5){
+              $sql = 'SELECT *
+              FROM movies
+              LEFT OUTER JOIN (
+                  SELECT COUNT(*) AS filmtotal FROM movies WHERE category_cat_id = :catid
+              ) AS T ON filmtotal > 0
+              WHERE category_cat_id = :catid
+              ORDER BY mov_add_date ASC
+              LIMIT 3
+              OFFSET '.$offset.'';
 
-        if ($pdoStatement->execute() === false ) {
-            print_r($pdoStatement->errorInfo());
-        }
-        else {
-            $AllResults = $pdoStatement->fetchall(PDO::FETCH_ASSOC);
-            echo json_encode($AllResults, JSON_PRETTY_PRINT);
-            // print_r($AllResults);
-        }
+              $pdoStatement = $pdo->prepare($sql);
 
+              $pdoStatement->bindValue(':catid', $categoryID);
+
+              if ($pdoStatement->execute() === false ) {
+                  print_r($pdoStatement->errorInfo());
+              }
+              else {
+                  $AllResults = $pdoStatement->fetchall(PDO::FETCH_ASSOC);
+                  echo json_encode($AllResults, JSON_PRETTY_PRINT);
+                  // print_r($AllResults);
+              }
+
+          }
+          else if ($validatorvalue === 6){
+              $sql = 'SELECT *
+              FROM movies
+              INNER JOIN category ON category_cat_id = cat_id
+              LEFT OUTER JOIN (
+                  SELECT COUNT(*) AS filmtotal
+                  FROM movies
+                  INNER JOIN category ON category_cat_id = cat_id
+                  WHERE mov_title LIKE :word
+                  OR mov_synopsis LIKE :word
+                  OR cat_name LIKE :word
+              ) AS T ON filmtotal > 0
+              WHERE mov_title LIKE :word
+              OR mov_synopsis LIKE :word
+              OR cat_name LIKE :word
+              ORDER BY mov_add_date ASC
+              LIMIT 3
+              OFFSET '.$offset.'';
+
+              $pdoStatement = $pdo->prepare($sql);
+
+              $pdoStatement->bindValue(':word', "%{$wordSearch}%");
+
+              if ($pdoStatement->execute() === false ) {
+                  print_r($pdoStatement->errorInfo());
+              }
+              else {
+                  $AllResults = $pdoStatement->fetchall(PDO::FETCH_ASSOC);
+                  echo json_encode($AllResults, JSON_PRETTY_PRINT);
+                  // print_r($AllResults);
+              }
+
+              // Calculer le nombre de résultats trouvés
+              $numberOfResults = 0;
+              foreach ($AllResults as $key => $value) {
+                  $numberOfResults += 1;
+              }
+              $phraseRecherche = $numberOfResults.' résultat(s) trouvé(s)';
+          }
+
+          if($validatorvalue !== 5 && $validatorvalue !== 6){
+
+              $ResultSet = $pdo->query($sql);
+
+
+              if($ResultSet === false){
+                  print_r($pdo->errorInfo());
+              }else{
+                  $AllResults = $ResultSet->fetchAll(PDO::FETCH_ASSOC);
+                  echo json_encode($AllResults, JSON_PRETTY_PRINT);
+              }
+
+          }
+      }
+      else{
+          $sql = 'SELECT *
+          FROM movies
+          LEFT OUTER JOIN (
+              SELECT COUNT(*) AS filmtotal FROM movies WHERE category_cat_id = :catfilter
+          ) AS T ON filmtotal > 0
+          WHERE category_cat_id = :catfilter
+          ORDER BY mov_add_date ASC
+          LIMIT 3
+          OFFSET '.$offset.'';
+
+          $pdoStatement = $pdo->prepare($sql);
+
+          $pdoStatement->bindValue(':catfilter', $CategoryFilter);
+
+          if ($pdoStatement->execute() === false ) {
+              print_r($pdoStatement->errorInfo());
+          }
+          else {
+              $AllResults = $pdoStatement->fetchall(PDO::FETCH_ASSOC);
+              echo json_encode($AllResults, JSON_PRETTY_PRINT);
+              // print_r($AllResults);
+          }
+
+      }
     }
 }
 
