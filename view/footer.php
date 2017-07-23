5fa95aa1d3d6e0b0b0959e0e9de129ab1cc108cb
Dany Lopes <d-lo@outlook.de>
2017-07-23 18:55:06 +0200

    Header et Footer terminé.

--- view/footer.php
+++ view/footer.php
@@ -1,9 +1,11 @@
         </main>
         <footer>
-          <p>© 2017 | Tous droits réservés</p>
-          <a href="<?=$socialLinksPage->facebook->shareUrl?>"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
-          <a href="<?=$socialLinksPage->linkedin->shareUrl?>"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-          <a href="<?=$socialLinksPage->plus->shareUrl?>"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+          <p>© Kevin Jesus, Dany Lopes 2017 | Tous droits réservés</p>
+          <div>
+            <a href="<?=$socialLinksPage->facebook->shareUrl?>"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+            <a href="<?=$socialLinksPage->linkedin->shareUrl?>"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+            <a href="<?=$socialLinksPage->plus->shareUrl?>"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+          </div>
         </footer>
     </body>
 </html>
